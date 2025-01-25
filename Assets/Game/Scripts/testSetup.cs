266a2c3cbf2dd77e35de7f9a004523f980a2a4b5
korrakot-impact360 <korrakot.intanon@impact360design.com>
2025-01-25 16:49:05 +0700

    test script setup on mac

---
+++ Assets/Game/Scripts/testSetup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testSetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var position = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

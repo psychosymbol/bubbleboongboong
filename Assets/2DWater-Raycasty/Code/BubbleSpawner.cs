b1ccdb1232f1bbd7f99a4d80e323afacf5e93038
psychosymbol <72127164+psychosymbol@users.noreply.github.com>
2025-01-25 17:42:07 +0700

    bubble spawner drag effect
    make it so if you drag the mouse while charging bubble, it'll spawn small bubble instead

--- Assets/2DWater-Raycasty/Code/BubbleSpawner.cs
+++ Assets/2DWater-Raycasty/Code/BubbleSpawner.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BubbleSpawner : MonoBehaviour
 {
     public GameObject bubblePrefab;
     [SerializeField]
     private GameObject currentBubble;
+    Vector3 lastMousePos;
+    Vector3 mouseDelta { get { return Input.mousePosition - lastMousePos; } }
     private void Update()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -17,6 +20,10 @@ public class BubbleSpawner : MonoBehaviour
 
             Rigidbody2D rb2d = currentBubble.GetComponent<Rigidbody2D>();
             rb2d.isKinematic = true;
+            lastMousePos = Input.mousePosition;
+
+            Collider2D bubbleCollider = rb2d.GetComponent<Collider2D>();
+            bubbleCollider.enabled = false;
         }
         //hold down mouse to make a bigger bubble
         if(Input.GetMouseButton(0))
@@ -25,7 +32,7 @@ public class BubbleSpawner : MonoBehaviour
             currentBubble.transform.position = mousePos;
             if(currentBubble.transform.localScale.x <1)
             {
-                Vector2 scaleValue = Vector2.one * Time.deltaTime * .1f;
+                Vector2 scaleValue = Vector2.one * Time.deltaTime * .5f;
                 Vector3 scaleUp = new Vector3(scaleValue.x, scaleValue.y, 0);
                 currentBubble.transform.localScale += scaleUp;
             }
@@ -33,6 +40,15 @@ public class BubbleSpawner : MonoBehaviour
             {
                 currentBubble.transform.localScale = Vector3.one;
             }
+            if (mouseDelta.magnitude > 5f)
+            {
+                Vector2 scaleValue = Vector2.one * .1f;
+                Vector3 scaleDown = new Vector3(scaleValue.x, scaleValue.y, 0);
+                currentBubble.transform.localScale -= scaleDown;
+                if (currentBubble.transform.localScale.x < .5f) currentBubble.transform.localScale = new Vector3(.5f, .5f, 1);
+                Instantiate(bubblePrefab, mousePos, Quaternion.identity);
+            }
+            lastMousePos = Input.mousePosition;
         }
         //release to fire off a bubble
         if(Input.GetMouseButtonUp(0))
@@ -40,6 +56,8 @@ public class BubbleSpawner : MonoBehaviour
             Debug.Log("ClickUp");
             Rigidbody2D rb2d = currentBubble.GetComponent<Rigidbody2D>();
             rb2d.isKinematic = false;
+            Collider2D bubbleCollider = rb2d.GetComponent<Collider2D>();
+            bubbleCollider.enabled = true;
             currentBubble = null;
         }
     }

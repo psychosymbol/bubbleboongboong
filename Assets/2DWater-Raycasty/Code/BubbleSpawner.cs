4da062ef14ce7f5232152be4edfca73a33f81ed2
psychosymbol <72127164+psychosymbol@users.noreply.github.com>
2025-01-25 12:48:47 +0700

    setup bubble spawner script
    setup bubble spawner script

--- Assets/2DWater-Raycasty/Code/BubbleSpawner.cs
+++ Assets/2DWater-Raycasty/Code/BubbleSpawner.cs
@@ -3,23 +3,44 @@ using UnityEngine;
 public class BubbleSpawner : MonoBehaviour
 {
     public GameObject bubblePrefab;
-    private void FixedUpdate()
+    [SerializeField]
+    private GameObject currentBubble;
+    private void Update()
     {
-        //hold down mouse to create bubble
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("ClickDown");
+            if (currentBubble == null)
+                currentBubble = Instantiate(bubblePrefab, mousePos, Quaternion.identity);
+
+            Rigidbody2D rb2d = currentBubble.GetComponent<Rigidbody2D>();
+            rb2d.isKinematic = true;
+        }
+        //hold down mouse to make a bigger bubble
         if(Input.GetMouseButton(0))
         {
-
+            Debug.Log("HoldClick");
+            currentBubble.transform.position = mousePos;
+            if(currentBubble.transform.localScale.x <1)
+            {
+                Vector2 scaleValue = Vector2.one * Time.deltaTime * .1f;
+                Vector3 scaleUp = new Vector3(scaleValue.x, scaleValue.y, 0);
+                currentBubble.transform.localScale += scaleUp;
+            }
+            else
+            {
+                currentBubble.transform.localScale = Vector3.one;
+            }
         }
-        //touch system
-        //else if()
-        //{
-        //
-        //}
-
         //release to fire off a bubble
         if(Input.GetMouseButtonUp(0))
         {
-
+            Debug.Log("ClickUp");
+            Rigidbody2D rb2d = currentBubble.GetComponent<Rigidbody2D>();
+            rb2d.isKinematic = false;
+            currentBubble = null;
         }
     }
 }

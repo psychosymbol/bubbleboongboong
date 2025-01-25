c1eb8a2ded6448c8423853f5b01bf3f6a411d7ad
korrakot-impact360 <korrakot.intanon@impact360design.com>
2025-01-26 02:24:11 +0700

    re-factor BubbleSpawner code and improve spawn behav

--- Assets/Game/Scripts/BubbleBehavior.cs
+++ Assets/Game/Scripts/BubbleBehavior.cs
@@ -38,6 +38,8 @@ public class BubbleBehavior : MonoBehaviour
         //    bubbleBehavior.BubblePop();
         //}
 
+        BubbleSpawner.instance.smallBubbles.Remove(gameObject);
+
         Destroy(gameObject);
     }
 }
--- Assets/Game/Scripts/BubbleSpawner.cs
+++ Assets/Game/Scripts/BubbleSpawner.cs
@@ -1,65 +1,129 @@
+using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class BubbleSpawner : MonoBehaviour
 {
+    public static BubbleSpawner instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
     public GameObject bubblePrefab;
     [SerializeField]
     private GameObject currentBubble;
     Vector3 lastMousePos;
     Vector3 mouseDelta { get { return Input.mousePosition - lastMousePos; } }
+
+    public List<GameObject> smallBubbles = new List<GameObject>();
+
+    public float maxBubbleSize = 2f;
+    public float minBubbleSize = .5f;
+    public float chargeUpSpeed = 0.5f;
+    public float chargeDownSpeed = 0.2f;
+    public float bubbleMassMultiplier = 0.1f;
+    public float smallSpawnDistance = 10; // px unit
+    public float currentSpawnDistance = 0f;
+
     private void Update()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0;
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("ClickDown");
-            if (currentBubble == null)
-                currentBubble = Instantiate(bubblePrefab, mousePos, Quaternion.identity);
-
-            Rigidbody2D rb2d = currentBubble.GetComponent<Rigidbody2D>();
-            rb2d.isKinematic = true;
-            lastMousePos = Input.mousePosition;
-
-            Collider2D bubbleCollider = rb2d.GetComponent<Collider2D>();
-            bubbleCollider.enabled = false;
+            //Debug.Log("ClickDown");
+            currentSpawnDistance = 0f;
+            CreateChargeBubble(mousePos);
         }
+
         //hold down mouse to make a bigger bubble
         if (Input.GetMouseButton(0))
         {
-            Debug.Log("HoldClick");
-            currentBubble.transform.position = mousePos;
-            if (currentBubble.transform.localScale.x < 2)
-            {
-                Vector2 scaleValue = Vector2.one * Time.deltaTime * .5f;
-                Vector3 scaleUp = new Vector3(scaleValue.x, scaleValue.y, 0);
-                currentBubble.transform.localScale += scaleUp;
-            }
-            else
-            {
-                currentBubble.transform.localScale = Vector3.one * 2;
-            }
-            if (mouseDelta.magnitude > 5f)
-            {
-                Vector2 scaleValue = Vector2.one * .1f;
-                Vector3 scaleDown = new Vector3(scaleValue.x, scaleValue.y, 0);
-                currentBubble.transform.localScale -= scaleDown;
-                if (currentBubble.transform.localScale.x < .5f) currentBubble.transform.localScale = new Vector3(.5f, .5f, 1);
-                Instantiate(bubblePrefab, mousePos, Quaternion.identity);
-            }
-            lastMousePos = Input.mousePosition;
+            //Debug.Log("HoldClick");
+            UpdateChargeBubble(mousePos);
         }
+
         //release to fire off a bubble
         if (Input.GetMouseButtonUp(0))
         {
-            Debug.Log("ClickUp");
-            Rigidbody2D rb2d = currentBubble.GetComponent<Rigidbody2D>();
-            rb2d.isKinematic = false;
-            rb2d.mass = currentBubble.transform.localScale.x / 10;
-            Collider2D bubbleCollider = rb2d.GetComponent<Collider2D>();
-            bubbleCollider.enabled = true;
-            currentBubble = null;
+            //Debug.Log("ClickUp");
+            ReleaseChargeBubble(mousePos);
+        }
+
+        lastMousePos = Input.mousePosition;
+    }
+
+    void CreateChargeBubble(Vector3 mousePos)
+    {
+        if (currentBubble != null) return;
+
+        currentBubble = Instantiate(bubblePrefab, mousePos, Quaternion.identity);
+
+        Rigidbody2D rb2d = currentBubble.GetComponent<Rigidbody2D>();
+        rb2d.isKinematic = true;
+        lastMousePos = Input.mousePosition;
+
+        Collider2D bubbleCollider = rb2d.GetComponent<Collider2D>();
+        bubbleCollider.enabled = false;
+    }
+
+    void ReleaseChargeBubble(Vector3 mousePos)
+    {
+        var currentBubbleScale = currentBubble.transform.localScale.x;
+        if (minBubbleSize > currentBubbleScale)
+        {
+            Destroy(currentBubble);
+            return;
+        }
+
+        Rigidbody2D rb2d = currentBubble.GetComponent<Rigidbody2D>();
+        Collider2D bubbleCollider = rb2d.GetComponent<Collider2D>();
+
+        rb2d.isKinematic = false;
+        rb2d.mass = currentBubbleScale * bubbleMassMultiplier;
+        bubbleCollider.enabled = true;
+        currentBubble = null;
+    }
+
+    void UpdateChargeBubble(Vector3 mousePos)
+    {
+        if (currentBubble == null) return;
+        var currentBubbleScale = currentBubble.transform.localScale.x;
+        currentBubble.transform.position = mousePos;
+
+        currentSpawnDistance += mouseDelta.magnitude;
+        if (currentSpawnDistance > smallSpawnDistance)
+        {
+            Vector2 scaleValue = Vector2.one * chargeDownSpeed;
+            currentBubble.transform.localScale -= (Vector3)scaleValue;
+            if (currentBubbleScale < minBubbleSize)
+                currentBubble.transform.localScale = new Vector3(minBubbleSize, minBubbleSize, 1);
+            ReleaseSmallBubble(mousePos);
+            currentSpawnDistance -= smallSpawnDistance;
+        }
+        else
+        {
+            if (currentBubbleScale < maxBubbleSize)
+            {
+                Vector2 scaleValue = chargeUpSpeed * Time.deltaTime * Vector2.one;
+                currentBubble.transform.localScale += (Vector3)scaleValue;
+            }
+            else
+            {
+                currentBubble.transform.localScale = Vector3.one * maxBubbleSize;
+            }
         }
     }
+
+    void ReleaseSmallBubble(Vector3 mousePos)
+    {
+        var smallbubble = Instantiate(bubblePrefab, mousePos, Quaternion.identity);
+        smallBubbles.Add(smallbubble);
+    }
+
 }

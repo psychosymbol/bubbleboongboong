4db4a3414933dfbab22c472f36e13888c233ffa4
psychosymbol <72127164+psychosymbol@users.noreply.github.com>
2025-01-25 12:06:58 +0700

    add buoncy to water
    add buoncy to water

---
+++ Assets/2DWater-Raycasty/Code/BubbleSpawner.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BubbleSpawner : MonoBehaviour
+{
+    public GameObject bubblePrefab;
+    private void FixedUpdate()
+    {
+        //hold down mouse to create bubble
+        if(Input.GetMouseButton(0))
+        {
+
+        }
+        //touch system
+        //else if()
+        //{
+        //
+        //}
+
+        //release to fire off a bubble
+        if(Input.GetMouseButtonUp(0))
+        {
+
+        }
+    }
+}
